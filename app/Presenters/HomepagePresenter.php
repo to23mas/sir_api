f6a7739e7d8b895a7340f11d53d12f22bb4be2e3
to23mas <to23mas@gmail.com>
2022-09-16 09:21:49 +0200

    testing basic JSON response

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -9,4 +9,23 @@ use Nette;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
+  public array $data = [
+    'ket' => 'value',
+    '1' => 1,
+    2 => 'hello',
+    'nested' => [
+      1 => 1,
+      2 => 2,
+      3 => [1,2,3,4,5,6,7,8,9]
+    ]
+  ];
+
+
+  public function actionDefault(): void
+  {
+    $this->sendJson([$this->data]);
+
+  }
+
 }
